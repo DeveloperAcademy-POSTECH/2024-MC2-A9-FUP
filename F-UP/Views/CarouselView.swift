aab10daa3b1bf68efc80c16b771e3314710dfa3e
00yhsp <00yhsp@naver.com>
2024-05-17 16:32:45 +0900

    [#11] 캐러셀 뷰 구현 완료

--- F-UP/Views/CarouselView.swift
+++ F-UP/Views/CarouselView.swift
@@ -11,24 +11,58 @@ struct CarouselView: View {
     var body: some View {
         GeometryReader { geometry in
             ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 20){
+                HStack(spacing: -20){
                     ForEach(1..<3, id: \.self) { index in
                         VStack(spacing: 0) {
                             Text("STEP \(index)")
                                 .padding(.top, 26)
                                 .font(.title)
                                 .fontWeight(.bold)
-                                .foregroundStyle(.white)
-                            Text("\(geometry.size.width)")
+                                .foregroundStyle(Theme.white)
+                            
+                            Text("오늘의 표현을 따라 말해보세요!")
+                                .padding(.top, 8)
+                                .font(.body)
+                                .fontWeight(.bold)
+                                .foregroundStyle(Theme.white)
+                            
+                            Text("🎤")
+                                .font(.system(size: 130))
+                                .padding(.top, 41)
+                                .padding(.bottom, 50)
+                            
+                            Button {
+                                //
+                            } label: {
+                                Text("따라 말하기")
+                                    .font(.headline)
+                                    .foregroundStyle(Theme.black)
+                                    .fontWeight(.bold)
+                                    .padding(.vertical, 14)
+                                    .frame(maxWidth: .infinity)
+                                    .background(Theme.white)
+                                    .clipShape(
+                                        RoundedRectangle(cornerRadius: Theme.round)
+                                    )
+                            }.padding(.horizontal, 20)
+                                .padding(.bottom, 20)
                         }
                         .containerRelativeFrame(.horizontal)
                         .scrollTargetBehavior(.viewAligned)
                         .frame(width: geometry.size.width - 74)
-//                        .safeAreaPadding(.horizontal, 40.0)
-                            .background(Color(uiColor: UIColor(red: 0.18, green: 0.73, blue: 0.7, alpha: 1)))
+                        .background(Theme.point)
+                        .clipShape(
+                            RoundedRectangle(cornerRadius: Theme.round)
+                        )
+                    }.scrollTransition(.animated, axis: .horizontal) { content, phase in
+                        content
+                            .opacity(phase.isIdentity ? 1.0 : 0.5)
+                            .scaleEffect(phase.isIdentity ? 1.0 : 0.8)
                     }
+
                 }
-            }
+            }.safeAreaPadding(.horizontal, 37)
+                .background(.red)
         }
     }
 }
