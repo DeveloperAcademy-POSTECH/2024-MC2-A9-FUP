16f1ab5bf1eb7e5ad7f3381c5de23ddcadc9f8a5
namudi <namudongs@gmail.com>
2024-05-17 15:15:40 +0900

    [#12] Theme 모델 생성(수정)

--- F-UP/Views/ChallengeView.swift
+++ F-UP/Views/ChallengeView.swift
@@ -10,7 +10,16 @@ import SwiftUI
 struct ChallengeView: View {
     var body: some View {
         ZStack {
-            Color.red.ignoresSafeArea()
+            Theme.background.ignoresSafeArea()
+            
+            VStack {
+                RoundedRectangle(cornerRadius: Theme.round)
+                    .fill(Theme.point)
+                    .frame(height: 200)
+                    .padding(Theme.padding)
+                    .dropShadow(opacity: 0.5)
+            }
+            
         }
     }
 }
