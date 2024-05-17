e79af2ee37dc223e11dd8cdf5ad69bb7c4879ed1
00yhsp <00yhsp@naver.com>
2024-05-17 15:25:58 +0900

    [#11] 캐러셀 구현중

---
+++ F-UP/Views/CarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  CarouselView.swift
+//  F-UP
+//
+//  Created by 박현수 on 5/17/24.
+//
+
+import SwiftUI
+
+struct CarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarouselView()
+}
