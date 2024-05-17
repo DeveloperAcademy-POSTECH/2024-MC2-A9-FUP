09e799bb272c55f7effbd77aa89bb17590bf77c4
leewanjae <dhkswo1@gmail.com>
2024-05-17 17:10:15 +0900

    [#17] 히스토리 UI 구현

---
+++ F-UP/Views/HistoryDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  HistoryDetailView.swift
+//  F-UP
+//
+//  Created by LeeWanJae on 5/17/24.
+//
+
+import SwiftUI
+
+struct HistoryDetailView: View {
+    var body: some View {
+        VStack {
+            Text("히스토리 디테일")
+        }
+    }
+}
+
+#Preview {
+    HistoryDetailView()
+}
