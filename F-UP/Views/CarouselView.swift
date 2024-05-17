e82ef227c971d1b94914e391def293904962bce6
00yhsp <00yhsp@naver.com>
2024-05-17 15:26:12 +0900

    [#11] 캐러셀 구현중

--- F-UP/Views/CarouselView.swift
+++ F-UP/Views/CarouselView.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct CarouselView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 20){
+                    ForEach(1..<3, id: \.self) { index in
+                        VStack(spacing: 0) {
+                            Text("STEP \(index)")
+                                .padding(.top, 26)
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .foregroundStyle(.white)
+                            Text("\(geometry.size.width)")
+                        }
+                        .containerRelativeFrame(.horizontal)
+                        .scrollTargetBehavior(.viewAligned)
+                        .frame(width: geometry.size.width - 74)
+//                        .safeAreaPadding(.horizontal, 40.0)
+                            .background(Color(uiColor: UIColor(red: 0.18, green: 0.73, blue: 0.7, alpha: 1)))
+                    }
+                }
+            }
+        }
     }
 }
 
