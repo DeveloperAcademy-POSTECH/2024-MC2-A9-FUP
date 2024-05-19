2329e50e7f312deedd3c12a2e603ff77f2f4a550
00yhsp <00yhsp@naver.com>
2024-05-20 01:47:32 +0900

    [#30] AVFoundation Manager 구현

--- F-UP/Manager/AVFoundationManager.swift
+++ F-UP/Manager/AVFoundationManager.swift
@@ -6,3 +6,120 @@
 //
 
 import Foundation
+import AVFoundation
+
+@Observable
+final class AVFoundationManager: NSObject {
+    private var audioRecorder: AVAudioRecorder?
+    private var audioPlayer: AVAudioPlayer?
+    private var recordingSession: AVAudioSession = AVAudioSession.sharedInstance()
+    
+    var isRecording = false
+    var isPlaying = false
+    var audioFilename: URL?
+    
+    override init() {
+        super.init()
+        setupSession()
+    }
+    
+    private func setupSession() {
+        do {
+            // 오디오 세션의 카테고리를 재생 및 녹음 모드로 설정
+            try recordingSession.setCategory(.playAndRecord, mode: .default)
+            // 오디오 세션 활성화
+            try recordingSession.setActive(true)
+        } catch {
+            print("Failed to setup recording session: \(error.localizedDescription)")
+        }
+    }
+    
+    func startRecording(fileName: String) {
+        if audioFilename != nil {
+            deleteRecording()
+        }
+        // 녹음 파일 경로 설정
+        let audioFilename = getDocumentsDirectory().appendingPathComponent("\(fileName).m4a")
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+            audioRecorder?.delegate = self
+            audioRecorder?.record()
+            
+            self.audioFilename = audioFilename
+            isRecording = true
+        } catch {
+            print("Could not start recording: \(error.localizedDescription)")
+        }
+    }
+    
+    func stopRecording() {
+        audioRecorder?.stop()
+        isRecording = false
+    }
+    
+    func playRecording() {
+        guard let audioFilename = audioFilename else { return }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audioFilename)
+            audioPlayer?.delegate = self
+            audioPlayer?.play()
+            
+            isPlaying = true
+        } catch {
+            print("Could not play audio: \(error.localizedDescription)")
+        }
+    }
+    
+    func stopPlaying() {
+        audioPlayer?.stop()
+        isPlaying = false
+    }
+    
+    func deleteRecording() {
+        guard let audioFilename = audioFilename else { return }
+        
+        do {
+            try FileManager.default.removeItem(at: audioFilename)
+            self.audioFilename = nil
+            print("Recording deleted successfully.")
+        } catch {
+            print("Could not delete recording: \(error.localizedDescription)")
+        }
+    }
+    
+    func getCurrentRecordingPath() -> URL? {
+        return audioFilename
+    }
+    
+    func setCurrentRecordingPath(_ path: URL) {
+        self.audioFilename = path
+    }
+    
+    private func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+}
+
+extension AVFoundationManager: AVAudioRecorderDelegate, AVAudioPlayerDelegate {
+    // 녹음이 완료되면 호출. 성공적으로 완료되지 않은 경우 stopRecording 메서드 호출
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            stopRecording()
+        }
+    }
+    // 재생이 완료되면 호출. 재생 상태를 false로 설정
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        if flag {
+            isPlaying = false
+        }
+    }
+}
--- F-UP/Views/ContentTabView.swift
+++ F-UP/Views/ContentTabView.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 
 struct ContentTabView: View {
+    
     init() {
-        UITabBar.appearance().backgroundColor = UIColor.white
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.backgroundColor = UIColor.white
+        tabBarAppearance.backgroundImage = UIImage()
+        tabBarAppearance.stackedLayoutAppearance.selected.iconColor = UIColor(red: 0.18, green: 0.73, blue: 0.7, alpha: 1)
+        tabBarAppearance.stackedLayoutAppearance.selected.titleTextAttributes = [
+            .foregroundColor: UIColor(red: 0.18, green: 0.73, blue: 0.7, alpha: 1)
+        ]
+        
+        UITabBar.appearance().standardAppearance = tabBarAppearance
+        // 기본 탭바 appearance
+        UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+        // 스크롤 시 탭바 appearance
     }
     
     var body: some View {
