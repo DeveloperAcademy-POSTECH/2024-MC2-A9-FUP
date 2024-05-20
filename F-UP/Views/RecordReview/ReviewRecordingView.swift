432c685caeb6c8f23724e769525d1ba0d8d5d48c
namudi <namudongs@gmail.com>
2024-05-20 15:20:09 +0900

    [#37] 탭뷰 backgroundColor 및 tintColor 변경

--- F-UP/Views/RecordReview/ReviewRecordingView.swift
+++ F-UP/Views/RecordReview/ReviewRecordingView.swift
@@ -12,97 +12,95 @@ struct ReviewRecordingView: View {
     @Binding var showModal: Bool
     
     var body: some View {
-        ZStack {
-            Theme.background
-                .ignoresSafeArea()
-            
-            GeometryReader { geo in
-                NavigationStack {
-                    VStack(alignment: .center, spacing: 0) {
-                        Text("오늘의 표현")
-                            .font(.footnote .weight(.regular))
-                            .foregroundColor(Theme.semiblack)
-                            .padding(.top, 34)
-                            .padding(.bottom, 3)
-                        Text("“오늘 하루도 정말 수고 많았어.”")
-                            .font(.title3 .weight(.bold))
-                            .foregroundColor(Theme.black)
-                            .padding(.bottom, 34)
-                        
+        NavigationStack {
+            ZStack {
+                Theme.background.ignoresSafeArea()
+                
+                VStack(alignment: .center, spacing: 0) {
+                    Text("오늘의 표현")
+                        .padding(.top, 34)
+                        .font(.footnote .weight(.regular))
+                        .foregroundColor(Theme.semiblack)
+                        .padding(.bottom, 3)
+                    Text("“오늘 하루도 정말 수고 많았어.”")
+                        .font(.title3 .weight(.bold))
+                        .foregroundColor(Theme.black)
+                        .padding(.bottom, 34)
+                    
+                    RoundedRectangle(cornerRadius: Theme.round)
+                        .fill(Theme.white)
+                        .frame(maxHeight: 418)
+                        .overlay {
+                            VStack {
+                                Spacer()
+                                Image("TempWaveImage")
+                                Spacer()
+                                Button {
+                                    // 녹음 재생 기능 구현
+                                } label: {
+                                    Label(
+                                        title: { Text("Play") },
+                                        icon: { Image(systemName: "play.fill") }
+                                    )
+                                }
+                                .controlSize(.small)
+                                .buttonStyle(.borderedProminent)
+                                .buttonBorderShape(.roundedRectangle(radius: 20))
+                                .tint(Theme.point)
+                                .padding(.bottom, 24)
+                            }
+                        }
+                        .padding(.bottom, 38)
+                    
+                    Button {
+                        dismiss()
+                    } label : {
                         RoundedRectangle(cornerRadius: Theme.round)
                             .fill(Theme.white)
-                            .frame(maxHeight: 418)
+                            .dropShadow(opacity: 0.1)
+                            .frame(width: 353, height: 50)
                             .overlay {
-                                VStack {
-                                    Spacer()
-                                    Image("TempWaveImage")
-                                    Spacer()
-                                    Button {
-                                        // 녹음 재생 기능 구현
-                                    } label: {
-                                        Label(
-                                            title: { Text("Play") },
-                                            icon: { Image(systemName: "play.fill") }
-                                        )
-                                    }
-                                    .controlSize(.small)
-                                    .buttonStyle(.borderedProminent)
-                                    .buttonBorderShape(.roundedRectangle(radius: 20))
-                                    .tint(Theme.point)
-                                    .padding(.bottom, 24)
-                                }
+                                Text("다시 녹음하기")
+                                    .font(.headline .weight(.bold))
+                                    .foregroundStyle(Theme.point)
                             }
-                            .padding(.bottom, 38)
-                        
-                        Button {
-                            dismiss()
-                        } label : {
-                            RoundedRectangle(cornerRadius: Theme.round)
-                                .fill(Theme.white)
-                                .dropShadow(opacity: 0.1)
-                                .frame(width: 353, height: 50)
-                                .overlay {
-                                    Text("다시 녹음하기")
-                                        .font(.headline .weight(.bold))
-                                        .foregroundStyle(Theme.point)
-                                }
-                                .padding(.bottom, 13)
-                        }
-                        
-                        Button {
-                            showModal = false
-                            
-                            // 녹음한 내용 저장하고 챌린지 단계 변경
-                            // ...
-                            
-                        } label : {
-                            RoundedRectangle(cornerRadius: Theme.round)
-                                .fill(Theme.point)
-                                .dropShadow(opacity: 0.2)
-                                .frame(width: 353, height: 50)
-                                .overlay {
-                                    Text("다음으로 넘어가기")
-                                        .font(.headline .weight(.bold))
-                                        .foregroundStyle(Theme.white)
-                                }
-                        }
+                            .padding(.bottom, 13)
                     }
-                    .padding(.horizontal, Theme.padding)
-                }
-                .navigationTitle("말하기")
-                .navigationBarTitleDisplayMode(.inline)
-                .navigationBarBackButtonHidden()
-                .toolbar {
-                    Button("취소") {
+                    
+                    Button {
                         showModal = false
+                        
+                        // 녹음한 내용 저장하고 챌린지 단계 변경
+                        // ...
+                        
+                    } label : {
+                        RoundedRectangle(cornerRadius: Theme.round)
+                            .fill(Theme.point)
+                            .dropShadow(opacity: 0.2)
+                            .frame(width: 353, height: 50)
+                            .overlay {
+                                Text("다음으로 넘어가기")
+                                    .font(.headline .weight(.bold))
+                                    .foregroundStyle(Theme.white)
+                            }
                     }
-                    .tint(Theme.point)
+                    Spacer()
                 }
+                .padding(.horizontal, Theme.padding)
+            }
+            .navigationTitle("말하기")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden()
+            .toolbar {
+                Button("취소") {
+                    showModal = false
+                }
+                .tint(Theme.point)
             }
         }
     }
 }
 
 #Preview {
-    ReviewRecordingView(showModal: .constant(true))
+        ReviewRecordingView(showModal: .constant(true))
 }
