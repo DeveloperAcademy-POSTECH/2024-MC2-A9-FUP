c26debbcbd52b125b0d23c947a1a1418503a9c73
00yhsp <00yhsp@naver.com>
2024-05-22 23:44:29 +0900

    [#62] 위젯 뷰 구현 완료

---
+++ ExpressionStreakWidget/ExpressionStreakWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ExpressionStreakWidgetBundle.swift
+//  ExpressionStreakWidget
+//
+//  Created by 박현수 on 5/22/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ExpressionStreakWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ExpressionStreakWidget()
+    }
+}
