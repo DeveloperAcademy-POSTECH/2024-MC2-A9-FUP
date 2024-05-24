c420fd07a3999451e4a70fd93b8a134d4a65e8bd
leewanjae <dhkswo1@gmail.com>
2024-05-24 14:30:31 +0900

    [#89] 로티 최종 수정

--- F-UP/App/F_UPApp.swift
+++ F-UP/App/F_UPApp.swift
@@ -46,7 +46,6 @@ struct F_UPApp: App {
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         NotificationManger.shared.setNotiAuth()
-        Thread.sleep(forTimeInterval: 1)
         return true
     }
 }
