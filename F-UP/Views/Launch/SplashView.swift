4f2968e3e67a30077907d6ace554af3d7131e20c
leewanjae <dhkswo1@gmail.com>
2024-05-24 10:15:30 +0900

    [#89] 로티 런치스크린
    [#89] 로티 런치스크린 리베이스 충돌 해결

---
+++ F-UP/Views/Launch/SplashView.swift
@@ -0,0 +1,40 @@
+//
+//  SplashView.swift
+//  F-UP
+//
+//  Created by LeeWanJae on 5/24/24.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    @State private var isSplashScreen = true
+    var body: some View {
+        ZStack {
+            Group {
+                if isSplashScreen {
+                    SplashScreen()
+                } else {
+                    ContentTabView()
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    withAnimation {
+                        isSplashScreen = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct SplashScreen: View {
+    var body: some View {
+        ZStack {
+            Color.white
+                .ignoresSafeArea()
+            LottieView(fileName: "Lunch")
+        }
+    }
+}
